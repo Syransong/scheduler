7e3cb17c3bd68735bbe76af2286b2331fe49ccb7
Syransong <ksysong@gmail.com>
2021-04-12 15:42:59 -0700

    create formatSpots fcn so all tests pass

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -4,6 +4,19 @@ import "components/DayListItem.scss";
 
 export default function DayListItem(props) {
 
+  const formatSpots = props => {
+
+    if (props.spots === 0) {
+      return "no spots remaining";
+    }
+
+    if (props.spots === 1) {
+      return "1 spot remaining";
+    }
+
+    return `${props.spots} spots remaining`;
+  } 
+
   const dayClass = classnames("day-list__item", {
     "day-list__item--selected": props.selected,
     "day-list__item--full": props.spots === 0
@@ -12,7 +25,7 @@ export default function DayListItem(props) {
   return (
     <li className={dayClass} onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{props.spots}</h3>
+      <h3 className="text--light">{formatSpots(props)}</h3>
     </li>
   );
 }
