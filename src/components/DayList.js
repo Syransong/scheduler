d9d1c8f2097b11c5909749570d9ba1b1a3542a9d
Syransong <ksysong@gmail.com>
2021-04-12 16:12:13 -0700

    remove comments

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -1,13 +1,8 @@
 import React from "react";
 import DayListItem from "./DayListItem";
-// import classnames from "classnames";
 
 export default function DayList(props) {
-  // const array = [] 
-  //3 props: days(array), day(string), setDay(function)
-
-  //const map = array.map(x => x * 2);
-  //array is props.days
+  
   return (
   <ul>
     {props.days.map(day => {
