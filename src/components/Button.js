ead24a3166dcf07903a9acf28b2664c6d1942f4f
Syransong <ksysong@gmail.com>
2021-04-12 14:03:11 -0700

    coding button in progress

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,5 +3,17 @@ import React from "react";
 import "components/Button.scss";
 
 export default function Button(props) {
-   return <>Button</>;
+  let buttonClass = "button";
+
+  if (props.confirm) {
+    buttonClass += " button--confirm";
+  }
+
+  return (
+    <button 
+      className={buttonClass}
+    >
+    {props.children}
+    </button>
+   );
 }
