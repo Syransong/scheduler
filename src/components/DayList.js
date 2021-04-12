47847d3a18fb9fe6ee8f4ffab60b4ca18e03da1a
Syransong <ksysong@gmail.com>
2021-04-12 16:11:51 -0700

    use map method to iterate thru days array and return components

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -1,7 +1,23 @@
 import React from "react";
+import DayListItem from "./DayListItem";
 // import classnames from "classnames";
 
 export default function DayList(props) {
+  // const array = [] 
+  //3 props: days(array), day(string), setDay(function)
 
-  return <ul></ul>;
+  //const map = array.map(x => x * 2);
+  //array is props.days
+  return (
+  <ul>
+    {props.days.map(day => {
+      return <DayListItem
+        name={day.name}
+        spots={day.spots}
+        selected={day.name === props.day}
+        setDay={props.setDay}
+      />
+    })}
+  </ul>
+  );
 }
