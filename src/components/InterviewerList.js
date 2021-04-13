5112293715fcf1f5d69a9eceff371ca796f6a969
Syransong <ksysong@gmail.com>
2021-04-13 15:19:10 -0700

    refactor to implement props

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,13 +1,25 @@
 import React from "react";
-// import classnames from "classnames";
 import "components/InterviewerList.scss";
+import InterviewerListItem from "./InterviewerListItem";
 
 export default function InterviewerList(props) {
 
+  const interviewers = props.interviewers.map(interviewer => {
+    return (<InterviewerListItem
+      key={interviewer.id}
+      name={interviewer.name}
+      avatar={interviewer.avatar}
+      selected={interviewer.id === props.interviewer}
+      setInterviewer={interviewer.setInterviewer}
+    />);
+  });
+  
   return (
     <section className="interviewers">
       <h4 className="interviewers__header text--light">Interviewer</h4>
-      <ul className="interviewers__list"></ul>
+      <ul className="interviewers__list">
+        {interviewers}
+      </ul>
     </section>
    );
 }
