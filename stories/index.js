e9bb7f8e0d1f91454aa7b340bddd8bee47dc71b1
Syransong <ksysong@gmail.com>
2021-04-13 17:10:22 -0700

    add Header story and refactor

--- stories/index.js
+++ stories/index.js
@@ -10,7 +10,8 @@ import DayListItem from "components/DayListItem";
 import DayList from "components/DayList";
 import InterviewerListItem from "components/InterviewerListItem";
 import InterviewerList from "components/InterviewerList";
-import Appointment from "components/Appointment/index"
+import Appointment from "components/Appointment/index";
+import Header from "components/Appointment/Header";
 
 storiesOf("Button", module)
   .addParameters({
@@ -139,6 +140,11 @@ storiesOf("Appointment", module)
   .add("Appointment", () => <Appointment />)
   .add("Appointment with Time", () => (
     <Appointment 
+      time="12pm"
+    />
+  ))
+  .add("Header", () => (
+    <Header
       time="12pm"
     />
   ));
