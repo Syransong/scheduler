e03315d98cd177676c9169cd6094a8536eeeef24
Syransong <ksysong@gmail.com>
2021-04-13 21:03:07 -0700

    refactor code to implement props

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -1,14 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
+import Button from "../Button";
+import InterviewerList from "../InterviewerList";
 
 export default function Form(props) {
- 
+
+  const [name, setName] = useState(props.name || "");
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
+
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
         <form autoComplete="off">
           <input
             className="appointment__create-input text--semi-bold"
-            name="name"
+            name={name}
             type="text"
             placeholder="Enter Student Name"
             /*
@@ -16,12 +21,17 @@ export default function Form(props) {
             */
           />
         </form>
-        <InterviewerList interviewers={props.interviewers} value={interviewer} onChange={setInterviewer} />
+        <InterviewerList 
+          interviewers={props.interviewers} 
+          value={interviewer} 
+          onChange={setInterviewer} 
+          // setInterviewer={event => props.setInterviewer(interviewer.id)}
+        />
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
-          <Button danger>Cancel</Button>
-          <Button confirm>Save</Button>
+          <Button danger onClick={props.onCancel}>Cancel</Button>
+          <Button confirm onClick={props.onSave}> Save</Button>
         </section>
       </section>
     </main>
