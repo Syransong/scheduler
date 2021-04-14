f8594710ecef04992d009a6e19431d6e1cae7318
Syransong <ksysong@gmail.com>
2021-04-14 15:04:59 -0700

    import Fragment and add appointment empty story

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,15 +1,15 @@
 import React from "react";
 import "components/Appointment/styles.scss";
 
-// import Header from "components/Appointment/Header";
-// import Empty from "components/Appointment/Empty";
-// import Show from "components/Appointment/Show";
-// import Confirm from "components/Appointment/Confirm";
-// import Status from "components/Appointment/Status";
+import Header from "components/Appointment/Header";
+import Empty from "components/Appointment/Empty";
+import Show from "components/Appointment/Show";
 
 export default function Appointment(props) {
  
   return (
-    <article className="appointment"></article>
+    <article className="appointment">
+
+    </article>
    );
 }
--- stories/index.js
+++ stories/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Fragment } from "react";
 
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
@@ -149,6 +149,12 @@ storiesOf("Appointment", module)
     <Appointment 
       time="12pm"
     />
+  ))
+  .add("Appointment Empty", () => (
+    <Fragment>
+      <Appointment id={1} time="12pm" />
+      <Appointment id="last" time="1pm" />
+    </Fragment>
   ))
   .add("Header", () => (
     <Header
