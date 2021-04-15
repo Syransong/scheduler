99bf5dadf4a5c6ed7fae3ab9cd34291e728cc716
Syransong <ksysong@gmail.com>
2021-04-14 20:10:28 -0700

    create and pass all tests for selector fcn

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,5 +1,20 @@
 export function getAppointmentsForDay(state, day) {
+  const result = [];
+  const aptID = [];
 
+  const aptDays = state.days;
+  const apts = state.appointments;
+
+  for (let dayObj of aptDays) {
+    if (dayObj.name === day) {
+      aptID.push(...dayObj.appointments);
+    }
+  }
+
+  for (let key in apts) {
+    if (aptID.includes(apts[key].id)) {
+      result.push(apts[key]);
+    }
+  }
+  return result;
 };
-
-//return an array of appointments for the given day 
