4e8cf33b0acd4eba6be6bab66d38abd5edecb0e2
Syransong <ksysong@gmail.com>
2021-04-15 16:56:15 -0700

    code passes Replace tests

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -5,16 +5,19 @@ export default function useVisualMode (initial) {
   const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([initial]);
 
-  function transition (nextMode) {
+  function transition (nextMode, replace = false) {
     setMode(nextMode);
-    setHistory(prevMode => [...prevMode, nextMode]); //spread the history and add the next mode in history 
+    
+    replace
+      ? setHistory(prev => [...prev])
+      : setHistory(prevMode => [...prevMode, nextMode]); //spread the history and add the next mode in history 
   };
   
   function back () {
-    console.log("history", history);
+    // console.log("history", history);
     if (history.length > 1) {
       history.pop();
-      console.log("popped history", history);
+      // console.log("popped history", history);
       setMode(history[history.length - 1]);
     }
   }
