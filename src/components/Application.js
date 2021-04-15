d418437817080c75fbd9326c1a0dc9c6f3c11808
Syransong <ksysong@gmail.com>
2021-04-14 21:01:35 -0700

    import helper fcn and iterate thru appointment data

--- src/components/Application.js
+++ src/components/Application.js
@@ -2,55 +2,9 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import DayList from "components/DayList";
 import Appointment from "components/Appointment/index";
+import getAppointmentsForDay from "../helpers/selectors";
 
 import "components/Application.scss";
-
-// const appointments = [
-//   {
-//     id: 1,
-//     time: "12pm",
-//   },
-//   {
-//     id: 2,
-//     time: "1pm",
-//     interview: {
-//       student: "Lydia Miller-Jones",
-//       interviewer: {
-//         id: 1,
-//         name: "Sylvia Palmer",
-//         avatar: "https://i.imgur.com/LpaY82x.png",
-//       }
-//     }
-//   },
-//   {
-//     id: 3,
-//     time: "2pm",
-//     interview: {
-//       student: "Kirby Kurbs",
-//       interviewer: {
-//         id: 3,
-//         name: "Mildred Nazir",
-//         avatar: "https://i.imgur.com/T2WwVfS.png",
-//       }
-//     }
-//   },
-//   {
-//     id: 4,
-//     time: "3pm",
-//   },
-//   {
-//     id: 5,
-//     time: "4pm",
-//     interview: {
-//       student: "Meta Knight",
-//       interviewer: {
-//         id: 4,
-//         name: "Cohana Roy",
-//         avatar: "https://i.imgur.com/FK8V841.jpg",
-//       }
-//     }
-//   },
-// ];
 
 export default function Application(props) {
   const [state, setState] = useState({
@@ -59,15 +13,11 @@ export default function Application(props) {
     appointments: {}
   })
 
-  const dailyAppointments = [];
-
   const setDay = day => {
     setState({...state, day})
   };
 
-  // const setDays = (days) => {
-  //   setState(prev => ({ ...prev, days }));
-  // };
+  const dailyAppointments = getAppointmentsForDay(state, state.day);
 
   useEffect(() => {
     const baseURL = "http://localhost:8001/api";
@@ -89,7 +39,6 @@ export default function Application(props) {
   return (
     <main className="layout">
       <section className="sidebar">
-        {/* Replace this with the sidebar elements during the "Project Setup & Familiarity" activity. */}
         <img
           className="sidebar--centered"
           src="images/logo.png"
