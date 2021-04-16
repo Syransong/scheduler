09119392fbf39f18f8d707e69c4e00c43de47adc
Syransong <ksysong@gmail.com>
2021-04-15 20:31:40 -0700

    update onAdd to call transition fcn

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -6,19 +6,20 @@ import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
 import useVisualMode from "hooks/useVisualMode";
 
-const EMPTY = "EMPTY";
-const SHOW = "SHOW";
-
-const { mode, transition, back } = useVisualMode(
-  props.interview ? SHOW : EMPTY
-);
 
 export default function Appointment(props) {
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+  const CREATE = "CREATE";
+  
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
  
   return (
     <article className="appointment">
       <Header time={props.time}/>
-      {mode === EMPTY && <Empty onAdd={() => console.log("Clicked onAdd")} />}
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
       {mode === SHOW && (
         <Show
           student={props.interview.student}
