183aac81a939f2e6c658ce09bfa68257823f7692
Syransong <ksysong@gmail.com>
2021-04-20 16:49:36 -0700

    all tests pass but interviewer name and student name do not display upon save

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -6,21 +6,32 @@ export default function Form(props) {
 
   const [name, setName] = useState(props.name || "");
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
+  const [error, setError] = useState("");
 
   const reset = () => {
     setName("");
     setInterviewer(null);
   };
 
-  const handleSubmit = () => {
-    const interviewerObj = props.interviewers.filter(x => x.id === interviewer)
-    props.onSave(name, interviewerObj[0])
-  };
+  // const handleSubmit = () => {
+  //   const interviewerObj = props.interviewers.filter(x => x.id === interviewer)
+  //   props.onSave(name, interviewerObj[0])
+  // };
 
   const cancel = () => {
     reset();
     props.onCancel();
   };
+
+  function validate() {
+    if (name === "") {
+      setError("Student name cannot be blank");
+      return;
+    }
+  
+    props.onSave(name, interviewer);
+    // handleSubmit();
+  }
 
   return (
     <main className="appointment__card appointment__card--create">
@@ -35,6 +46,7 @@ export default function Form(props) {
             data-testid="student-name-input"
           />
         </form>
+        <section className="appointment__validation">{error}</section>
         <InterviewerList 
           interviewers={props.interviewers} 
           value={interviewer} 
@@ -44,7 +56,7 @@ export default function Form(props) {
       <section className="appointment__card-right">
         <section className="appointment__actions">
           <Button danger onClick={() => cancel()}>Cancel</Button>
-          <Button confirm onClick={handleSubmit}> Save</Button>
+          <Button confirm onClick={validate}>Save</Button>
         </section>
       </section>
     </main>
