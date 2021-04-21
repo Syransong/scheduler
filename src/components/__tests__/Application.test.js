caefef63df93042c9535361760ba16bc59342b8a
Syransong <ksysong@gmail.com>
2021-04-20 22:29:24 -0700

    edit fcn text passes, number of spots does not change upon edit

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -90,4 +90,41 @@ describe("Application", () => {
     
     debug();
   });
+
+  it("loads data, edits an interview and keeps the spots remaining for Monday the same", async() => {
+    // 1. Render the Application.
+    const { container, debug } = render(<Application />);
+
+    // 2. Wait until the text "Archie Cohen" is displayed.
+    await waitForElement(() => getByText(container, "Archie Cohen"));
+
+    // 3. Click the "Edit" button on the booked appointment.
+    const appointment = getAllByTestId(container, "appointment")
+      .find(appointment => queryByText(appointment, "Archie Cohen"));
+
+    fireEvent.click(getByAltText(appointment, "Edit"));
+
+    // 5. Edit the interview 
+    fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
+      target: { value: "Jason Mason" }
+    })
+
+    fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
+
+    // 6. Click the "Save" button on the confirmation.
+    fireEvent.click(getByText(appointment, "Save"));
+
+    // 6. Check that the element with the text "Saving!" is displayed.
+    expect(getByText(appointment, "Saving!")).toBeInTheDocument();
+
+    // 7. Wait until the element with the text "Jason Mason" is displayed.
+    await waitForElement(() => getByText(appointment, "Jason Mason"));
+
+    // 8. Check that the DayListItem with the text "Monday" also has the text "2 spots remaining".
+    const day = getAllByTestId(container, "day").find(day =>
+      queryByText(day, "Monday")
+    );
+    
+    expect(getByText(day, "1 spot remaining")).toBeInTheDocument();
+  });
 });
