7146cfe5a25e6290d02fa51b9bceed1275bcdf22
Syransong <ksysong@gmail.com>
2021-04-20 19:37:54 -0700

    wrap all tests with a describe block

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -6,11 +6,14 @@ import Application from "components/Application";
 
 afterEach(cleanup);
 
-it("defaults to Monday and changes the schedule when a new day is selected", () => {
-  const { getByText } = render(<Application />);
-
-  return waitForElement(() => getByText("Monday")).then(() => {
-    fireEvent.click(getByText("Tuesday"));
-    expect(getByText("Leopold Silvers")).toBeInTheDocument();
+describe("Application", () => {
+  it("defaults to Monday and changes the schedule when a new day is selected", () => {
+    const { getByText } = render(<Application />);
+  
+    return waitForElement(() => getByText("Monday")).then(() => {
+      fireEvent.click(getByText("Tuesday"));
+      expect(getByText("Leopold Silvers")).toBeInTheDocument();
+    });
   });
+
 });
