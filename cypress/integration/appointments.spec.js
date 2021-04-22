e33a21845aab45a4ba054091153b34a0bb9a50e1
Syransong <ksysong@gmail.com>
2021-04-21 19:58:31 -0700

    create and pass cypress test for editing apt

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -25,4 +25,22 @@ describe("Appointments", () => {
     cy.contains(".appointment__card--show", "Sylvia Palmer");
   });
 
+  it("should edit an interview", () => {
+    cy.get("[alt=Edit]")
+      .first()
+      .click({ force: true});
+    
+    cy.get('[data-testid=student-name-input]')
+      .clear()
+      .type('Jason Mason');
+
+    cy.get("[alt='Tori Malcolm']")
+      .click();
+
+    cy.contains("Save")
+      .click();
+    
+    cy.contains(".appointment__card--show", "Jason Mason");
+    cy.contains(".appointment__card--show", "Tori Malcolm"); 
+  });
 });
