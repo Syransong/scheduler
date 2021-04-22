f66bda2def51ffcb7575386946a5bc73038b9f53
Syransong <ksysong@gmail.com>
2021-04-21 20:05:25 -0700

    create and pass cypress test for cancelling apt

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -43,4 +43,22 @@ describe("Appointments", () => {
     cy.contains(".appointment__card--show", "Jason Mason");
     cy.contains(".appointment__card--show", "Tori Malcolm"); 
   });
+
+  it("should cancel an interview", () => {
+    cy.get("[alt=Delete]")
+      .first()
+      .click({ force: true});
+    
+    cy.contains("Confirm")
+      .click();
+    
+    cy.contains("Deleting!")
+      .should("exist");
+    
+    cy.contains("Deleting!")
+      .should("not.exist");
+    
+    cy.contains(".appointment__card--show", "Archie Cohen")
+      .should("not.exist");
+  });
 });
